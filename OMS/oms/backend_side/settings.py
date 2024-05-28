d1bd50280de75bbef6c6751a5111091abc61bffd
Christian Q. Cleofas <cleofastiano27@gmail.com>
2024-05-28 16:30:30 +0800

    NO_TICKET - Try use Render

--- OMS/oms/backend_side/settings.py
+++ OMS/oms/backend_side/settings.py
@@ -1,6 +1,7 @@
 from decouple import config, Csv
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -58,15 +59,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend_side.wsgi.application'
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT', cast=int),
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 AUTH_PASSWORD_VALIDATORS = [
